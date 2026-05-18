cd22976459d3ffa34d0ab450bd5cfb7f0c12fb6d
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-18 22:07:16 +0700

    added layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,19 @@
-export default function RootLayout() {
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Harry Portfolio",
+  description: "Harry Nguyen's personal portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html>
-      <body></body>
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   );
 }
