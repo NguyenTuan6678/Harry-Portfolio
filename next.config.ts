f4e33e581dbe94be849d2cb787b904084e9c179e
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-07-01 14:14:30 +0700

    chore: update Next.js config (enable react compiler, disable dev indicators, allow dev origin)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   reactCompiler: true,
+  allowedDevOrigins: ["192.168.1.185"],
 };
 
 export default nextConfig;
